2515548389fb8a4f3abbbd771b1b58ee6841cbb1
B3silva <160868133+B3silva@users.noreply.github.com>
2025-02-25 09:24:39 -0300

    xhbhbx

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 document.addEventListener(`DOMContentLoaded` , function(){
-const aumentaFonteBotao = document .get this.ELE
+const aumentaFonteBotao = document.getElementById(aumentar-fonte);
 
-}
+let tamanhoAtualFonte = 1;
+
+aumentaFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = $ {tamanhoAtualFonte}rem
+
+})
