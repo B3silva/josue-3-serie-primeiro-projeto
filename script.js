b5c56e3c5e13a20e0beeb475ed464d02367af6bd
B3silva <160868133+B3silva@users.noreply.github.com>
2025-02-27 11:10:09 -0300

    dsfs

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let tamanhoAtualFonte = 1;
 
 aumentaFonteBotao.addEventListener('click', function(){
     tamanhoAtualFonte += 0.1;
-    document.body.style.fontSize = $ {tamanhoAtualFonte}rem
+    document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+
+})
 
 })
